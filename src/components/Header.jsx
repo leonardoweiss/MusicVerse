1e661863becf492c35672000034cd8e70814fe8b
Leonardo Weiss <lsweissleo@gmail.com>
2024-03-19 21:52:13 -0300

    Fixing responsivity problem in 'music-info' component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,6 @@ export default function Header() {
             <div id="logo">
                 <h2>MusicVerse</h2>
             </div>
-            {/* <nav>
-                <Link href="#">Início</Link>
-                <Link href="#">Artistas</Link>
-                <Link href="#">Gêneros</Link>
-                <Link href="#">Tendências</Link>
-            </nav> */}
             <div id="actions">
                 <button id="login">Login</button>
                 <button id="signUp">Cadastrar-se</button>
